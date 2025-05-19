603fe1f8316eeb78389998680e83743173acd95e
kasix44 <piesfuks@gmail.com>
2025-05-19 20:48:39 +0200

    zmiana connectionstring

--- obj/Debug/net8.0/WorkshopManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkshopManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb8be482daef959f3a91c6b30a8503811a6a78e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbc4247448ef67c40823ab85b7f1c517e80e8ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkshopManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
